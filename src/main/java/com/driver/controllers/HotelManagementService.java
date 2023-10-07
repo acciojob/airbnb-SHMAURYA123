2f915d7318f489c92db92f5f930786075792dbd1
Sourabh Maurya <shmaurya2014.com>
2023-10-07 21:02:39 +0530

    removed autwired

--- src/main/java/com/driver/controllers/HotelManagementService.java
+++ src/main/java/com/driver/controllers/HotelManagementService.java
@@ -11,8 +11,8 @@ import java.util.List;
 @Service
 public class HotelManagementService {
 
-    @Autowired
-    HotelManagementRepository hmr;
+
+    HotelManagementRepository hmr=new HotelManagementRepository();
     public String addHotel(Hotel hotel) {
         return hmr.addHotel(hotel);
     }
